9bea27f566c068869617c56eb103ab6f41132e2d
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-13 14:44:31 -0800

    nameprinter

---
+++ exercise/Chapter 1/src/NameInsideBox.java
@@ -0,0 +1,8 @@
+// 1.5 Print your name in a box
+public class NameInsideBox {
+    public static void main(String[] args){
+        System.out.println("+-------+");
+        System.out.println("| Cedar |");
+        System.out.println("+-------+");
+    }
+}
