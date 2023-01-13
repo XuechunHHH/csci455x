f5bb298bcbc4209ffc4cca942558daa19e087547
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-13 14:50:58 -0800

    using message dialog

---
+++ exercise/Chapter 1/src/MessageDialog.java
@@ -0,0 +1,7 @@
+// 1.11 Using message dialog
+import javax.swing.JOptionPane;
+public class MessageDialog {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hello, Cedar!");
+    }
+}
