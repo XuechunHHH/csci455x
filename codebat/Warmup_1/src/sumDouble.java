db4e0ababa40048132009ebe243023e1d9e9363f
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-14 21:24:51 -0800

    sumDouble

---
+++ codebat/Warmup_1/src/sumDouble.java
@@ -0,0 +1,10 @@
+public class sumDouble {
+    public static void main(String[] args) {
+        System.out.println(sumDouble(1, 2));
+    }
+
+    public static int sumDouble(int a, int b){
+        if (a ==  b) return (a+b) * 2;
+        else return a+b;
+    }
+}
