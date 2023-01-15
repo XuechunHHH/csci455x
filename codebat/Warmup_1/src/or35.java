5b2514db04520ac0b8087a3f76ca0c141c7d5a15
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-14 22:42:12 -0800

    or35

---
+++ codebat/Warmup_1/src/or35.java
@@ -0,0 +1,9 @@
+public class or35 {
+    public static void main(String[] args) {
+        System.out.println(or35(8));
+    }
+
+    public static boolean or35(int n){
+        return n % 3 == 0 || n % 5 ==0;
+    }
+}
