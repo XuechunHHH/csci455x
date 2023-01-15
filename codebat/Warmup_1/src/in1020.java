869cc76aa7e23020c85ddddecf642351d036fd25
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-14 22:54:03 -0800

    in1020

---
+++ codebat/Warmup_1/src/in1020.java
@@ -0,0 +1,9 @@
+public class in1020 {
+    public static void main(String[] args) {
+        System.out.println(in1020(8, 99));
+    }
+
+    public static boolean in1020(int a, int b){
+        return a>=10 && a<=20 || b>=10 && b<=20;
+    }
+}
