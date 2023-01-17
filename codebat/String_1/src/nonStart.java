60428b96fd49c557695c63534a1a77be95b976f9
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-16 23:57:38 -0800

    nonStart

---
+++ codebat/String_1/src/nonStart.java
@@ -0,0 +1,9 @@
+public class nonStart {
+    public static void main(String[] args) {
+        System.out.println(nonStart("shotl", "java"));
+    }
+
+    public static String nonStart(String a, String b){
+        return a.substring(1)+b.substring(1);
+    }
+}
