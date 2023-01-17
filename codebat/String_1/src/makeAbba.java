9cca939c24e0febba5ae7b0d66f4172a87bca737
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-16 23:42:23 -0800

    makeAbba

---
+++ codebat/String_1/src/makeAbba.java
@@ -0,0 +1,9 @@
+public class makeAbba {
+    public static void main(String[] args) {
+        System.out.println(makeAbba("What", "Up"));
+    }
+
+    public static String makeAbba(String a, String b){
+        return a+b+b+a;
+    }
+}
