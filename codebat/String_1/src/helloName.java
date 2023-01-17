8a5124f8ad0f1db77e7a9211b059d13ccdbcaad2
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-16 23:38:40 -0800

    helloName

---
+++ codebat/String_1/src/helloName.java
@@ -0,0 +1,9 @@
+public class helloName {
+    public static void main(String[] args) {
+        System.out.println(helloName("Alice"));
+    }
+
+    public static String helloName(String name){
+        return "Hello "+name+"!";
+    }
+}
