0dda6ebd1b60a36b52a9e282e2fa54e02e267321
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-16 23:58:56 -0800

    left2

---
+++ codebat/String_1/src/left2.java
@@ -0,0 +1,9 @@
+public class left2 {
+    public static void main(String[] args) {
+        System.out.println(left2("java"));
+    }
+
+    public static String left2(String str){
+        return str.substring(2)+str.substring(0,2);
+    }
+}
