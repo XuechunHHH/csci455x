4ff4404270ff00f46363c60ce11cc83934e6f56b
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-17 16:12:48 -0800

    LastTwo

---
+++ codebat/String_1/src/LastTwo.java
@@ -0,0 +1,13 @@
+public class LastTwo {
+    public static void main(String[] args) {
+        System.out.println(lastTwo("coding"));
+    }
+
+    public static String lastTwo(String str){
+        if (str.length() < 2) {
+            return str;
+        }else{
+            return str.substring(0,str.length()-2)+str.charAt(str.length()-1)+str.charAt(str.length()-2);
+        }
+    }
+}
