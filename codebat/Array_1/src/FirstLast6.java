69700425d90ac2b3ec14c6aa7261454b775b40e4
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-27 00:06:33 -0800

    FirstLast6

---
+++ codebat/Array_1/src/FirstLast6.java
@@ -0,0 +1,10 @@
+public class FirstLast6 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,6};
+        System.out.println(firstLast6(nums));
+    }
+
+    public static boolean firstLast6(int[] nums){
+        return (nums[0] == 6 || nums[nums.length-1] == 6);
+    }
+}
