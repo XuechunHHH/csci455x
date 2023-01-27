ba71fcfb1aa4ebce740b7a0dee07066ee9e83ee4
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-27 00:13:41 -0800

    MakePi

---
+++ codebat/Array_1/src/MakePi.java
@@ -0,0 +1,10 @@
+public class MakePi {
+    public static void main(String[] args) {
+        System.out.println(makePi());
+    }
+
+    public static int[] makePi(){
+        int[] pi = {3,1,4};
+        return pi;
+    }
+}
