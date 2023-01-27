a49861b14cee530d3db5254b04cba0b8e05e6f2a
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-27 00:25:27 -0800

    MaxEnd3

--- codebat/Array_1/src/MaxEnd3.java
+++ codebat/Array_1/src/MaxEnd3.java
@@ -1,14 +1,15 @@
-public class Reverse3 {
+public class MaxEnd3 {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
-        int[] res = reverse3(nums);
+        int[] res = maxEnd3(nums);
         for(int i=0;i<res.length;++i){
             System.out.println(res[i]);
         }
     }
 
-    public static int[] reverse3(int[] nums){
-        int[] res = {nums[2],nums[1],nums[0]};
+    public static int[] maxEnd3(int[] nums){
+        int num = Math.max(nums[0],nums[2]);
+        int[] res = {num,num,num};
         return res;
     }
 }
