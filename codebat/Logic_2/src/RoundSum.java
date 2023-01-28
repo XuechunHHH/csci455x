12d4bd2d712f6f5aeff8494e4febc2a5a69c87ca
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-28 11:20:32 -0800

    RountSum

---
+++ codebat/Logic_2/src/RoundSum.java
@@ -0,0 +1,14 @@
+public class RoundSum {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static int roundSum(int a, int b, int c){
+        return round10(a)+round10(b)+round10(c);
+    }
+
+    public static int round10(int num){
+        if (num%10>=5) return (num/10+1)*10;
+        return num/10*10;
+    }
+}
