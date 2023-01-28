5fb25db018e4ed6d118a5ec577c2ffe096a7867e
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-27 23:28:26 -0800

    TeenSum

---
+++ codebat/Logic_1/src/TeenSum.java
@@ -0,0 +1,10 @@
+public class TeenSum {
+    public static void main(String[] args) {
+        System.out.println(teenSum(10, 13));
+    }
+
+    public static int teenSum(int a, int b){
+        if (a>=13&&a<=19 || b>=13&&b<=19) return 19;
+        return a+b;
+    }
+}
