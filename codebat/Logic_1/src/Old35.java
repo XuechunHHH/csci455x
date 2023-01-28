f629c888b41b3a9ee28c7028f80a0ff19b48b011
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-01-27 21:48:33 -0800

    Old35

---
+++ codebat/Logic_1/src/Old35.java
@@ -0,0 +1,9 @@
+public class Old35 {
+    public static void main(String[] args) {
+        System.out.println(old35(15));
+    }
+
+    public static boolean old35(int n){
+        return n%3==0 && n%5!=0 || n%3!=0 && n%5==0;
+    }
+}
